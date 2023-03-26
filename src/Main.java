d4125cda12c086ca7c33d3ce1e091ce064e8608d
fakhrullhafiz <fakhrulhafiz99@gmail.com>
2023-03-26 15:47:24 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String args[]) {
+        Database db = new Database(
+                "db4free.net", "3306",
+                "ooputpjan23",
+                "nordin", "nrdinutp");
+        db.connect();
+        db.runQuery("select * from Student");
+
+    }
+}
